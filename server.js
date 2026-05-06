447dffaefd4d23abf9a7bec4a0cec5cec9874056
Umaru Bayoh <bayohumau66@gmail.com>
2026-05-06 18:56:59 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+// Import file
+import { fileURLToPath } from "url";
+
+// Import path
+import path from "path";
+
+// Import the express package
+import express from "express";
+
+// Defines the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLocaleLowerCase() || "production";
+
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+// Create filename
+const __filename = fileURLToPath(import.meta.url);
+
+// Create directory
+const __dirname = path.dirname(__filename);
+
+// Create the express app
+const app = express();
+
+/* -------------------------------
+* Configure Express middleware
+---------------------------------- */
+// Server static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+
+// Tell Express where to find your templates
+app.set("views", path.join(__dirname, "src/views"));
+
+
+/* -------------------------------
+* Routes
+---------------------------------- */
+// Home
+app.get("/", async(req, res) => {
+    const title = "Home"
+    res.render("home", { title });
+});
+
+// Organizations
+app.get("/organizations", async(req, res) => {
+    const title = "Our Partner Organizations"
+    res.render("organizations", { title });
+});
+
+// Projects
+app.get("/projects", async(req, res) => {
+    const title = "Service Projects"
+    res.render("projects", { title });
+});
+
+
+// Log app operation
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`)
+    console.log(`Environment: ${NODE_ENV}`)
+});
