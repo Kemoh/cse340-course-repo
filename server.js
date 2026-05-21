4c505274057d91c8d3bd326d98d4394dc0e1590b
Umaru Bayoh <bayohumau66@gmail.com>
2026-05-21 14:46:43 +0000

    W02 Learning Activity: Displaying Database results in EJS

--- server.js
+++ server.js
@@ -53,10 +53,9 @@ app.get("/", async(req, res) => {
 // Organizations
 app.get("/organizations", async(req, res) => {
     const organizations = await getAllOrganizations();
-    console.log(organizations);
 
     const title = "Our Partner Organizations";
-    res.render("organizations", { title });
+    res.render("organizations", { title, organizations });
 });
 
 // Projects
