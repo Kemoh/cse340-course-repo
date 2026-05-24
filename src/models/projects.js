770aad8fb664ba395dc6a310fd7b4a1c59fbb62d
Umaru Bayoh <bayohumau66@gmail.com>
2026-05-24 17:51:35 +0000

    W03 Learning Activity: Route and Query Parameters

--- src/models/projects.js
+++ src/models/projects.js
@@ -11,14 +11,38 @@ const getAllProjects = async () => {
             p.date,
             o.organization_id,
             o.name AS organization_name
-            FROM public.project p
-            JOIN organization o
-                ON p.organization_id = o.organization_id
-                ORDER BY p.date ASC;
+        FROM project p
+        JOIN organization o
+        ON p.organization_id = o.organization_id
+        ORDER BY p.date ASC;
     `;
-
+    
     const result = await db.query(query);
     return result.rows;
 };
 
-export { getAllProjects };
+
+const getProjectsByOrganizationId = async (organizationId) => {
+    const query = `
+        SELECT
+            project_id,
+            organization_id,
+            title,
+            description,
+            location,
+            date
+        FROM project
+        WHERE organization_id = $1
+        ORDER BY date; 
+    `;
+
+    const queryParams = [organizationId];
+    const result = await db.query(query, queryParams);
+    return result.rows;
+};
+
+// Export the model functions
+export { 
+    getAllProjects,
+    getProjectsByOrganizationId
+ };
